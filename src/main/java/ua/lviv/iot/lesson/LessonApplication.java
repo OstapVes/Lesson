f048dea2c3f793c5e9065b944bc53648875c9e4c
OstapVes <ostapvesolovskiy@gmail.com>
2021-05-20 14:22:40 +0300

    Lesson

---
+++ src/main/java/ua/lviv/iot/lesson/LessonApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ua/lviv/iot/lesson/exception/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package ua.lviv.iot.lesson.exception;
+
+public class OrderNotFoundException extends Exception {
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
